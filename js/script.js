db07245e777663eae318fa3e7609b931f3f85fad
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-12 15:55:51 +0200

    aggiungere immagine da data

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ const { createApp } = Vue;
 createApp({
   data(){
     return {
-      message: 'Hello Vue!!'
+      message: 'Hello Vue!!',
+      image: 'https://salumigombitelli.it/wp-content/uploads/2017/03/fabian-blank-67318-1.jpg'
     }
   }
 }).mount('#app')
 
 
 
-console.log('ciao')
+console.log('log di Vue stesso', Vue)
