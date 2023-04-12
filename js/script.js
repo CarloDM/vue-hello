c506db73bd330e50f4fe499f98b69918c6e8316d
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-12 17:02:17 +0200

    continuo esperimenti

--- js/script.js
+++ js/script.js
@@ -6,21 +6,27 @@ createApp({
       message: 'Hello Vue!!',
       image: 'https://salumigombitelli.it/wp-content/uploads/2017/03/fabian-blank-67318-1.jpg',
       stileFoto: ['width: 60%; dispaly: block', ' dispaly: block'], /* ???? perchè non funziona */
+      classeFoto: 'disp_img',
 
-      userMessage:'scrive utente'
+      userMessage:'scrive utente',
+      messEnter:'',
     }
   },
+
+
   methods: {
     saluta: function() {
       return this.message = 'ciao!!!',
               console.log('hai cliccato saluta')
     },
-    tastiera(){
-      
+    tastiera(event){
+      console.log('press:', event.key)
+    },
+    invio(event){
+      console.log('press enter:', event);
+      this.messEnter = this.userMessage;
     }
   }
 }).mount('#app')
-
-
 
 console.log('log di Vue stesso', Vue)
